c6690b1076af80312660696bd3673381bb0d5e78
vinaykumar-1nc22cd062 <shelbyyyy0502@gmail.com>
2024-07-03 11:52:10 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,10 @@
+nclude<stdio.h>
+int main()
+{
+    int m,h,v,g,e;
+    printf("Enter the m,h,g,v");
+    scanf("%d%d%d%d,&m,&h,&g,&v,");
+    E=m*g*h*+0.5*m*v*v*;
+    printf("E,%d\n,E");
+    return(0)
+}
